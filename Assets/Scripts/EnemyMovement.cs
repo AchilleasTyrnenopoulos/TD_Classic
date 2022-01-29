7c090a6bd965b418a8aeacd5bcb28a810a6d3c58
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2022-01-29 23:55:14 +0200

    Enemy
    - Import Melee animations package
    - Enemy movement
    - Enemy Animator & run animation

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,7 +7,8 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private List<Waypoint> path = new List<Waypoint>();
     [SerializeField]
-    private float waitTime = 1f;
+    [Range(0f, 10)]
+    private float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,9 +26,34 @@ public class EnemyMovement : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = new Vector3(waypoint.transform.position.x, this.transform.position.y, waypoint.transform.position.z);
+            Vector3 startPos = transform.position;
+            Vector3 endPos = waypoint.transform.position;
+            float travelPercent = 0f;
 
-            yield return new WaitForSeconds(waitTime);
+            float rotationDegrees = DegreesToRotate(this.transform.forward, startPos, endPos);
+
+            while(travelPercent < 1)
+            {
+                //rotate enemy
+                if(travelPercent < 0.25f)
+                {
+                    float rotationAngle = 4 * rotationDegrees * Time.deltaTime * speed;
+                    this.transform.Rotate(new Vector3(0, rotationAngle, 0));
+                }
+
+                //move enemy
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPos, endPos, travelPercent);                
+
+                yield return new WaitForEndOfFrame();
+            }
+            //transform.position = new Vector3(waypoint.transform.position.x, this.transform.position.y, waypoint.transform.position.z);            
         }
+    }
+
+    private float DegreesToRotate(Vector3 currentDir, Vector3 currentPosition, Vector3 endPosition)
+    {
+        Vector3 targetDir = endPosition - currentPosition;
+        return Vector3.SignedAngle(currentDir, targetDir, Vector3.up);
     }
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private bool isPlaceable;
+
+    private void OnMouseOver()
     {
-        
+        if(isPlaceable)
+            print(transform.name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
