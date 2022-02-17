66c65be9b16797170d4c94c38da10f9d4b7bc7da
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2022-02-17 23:52:32 +0200

    Update Gate prefab & create IHealth interface
    - Assign Ignore Raycast layer to Gate sensors
    - Gate arrows particle system
    - Create IHealth interface & EnemyHealth class

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour, IHealth
+{
+    public int MaxHealth { get => maxHealth;}
+    [SerializeField] private int maxHealth = 10;
+    public int CurrentHealth { get => currentHealth;  }
+    [SerializeField] private int currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject go)
+    {
+        TakeDamage(1);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+
+        if (currentHealth <= 0)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/IHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHealth 
+{
+    int MaxHealth { get;}
+    int CurrentHealth { get;}
+    void TakeDamage(int dmg);
+
+}
